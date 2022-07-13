4e1eb2e3275c988edab8594a7e6fc1b7fea6b4d7
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-07-13 15:08:35 -0300

    Project created

---
+++ backend/src/main/java/com/jaas/jaasmeta/JaasmetaApplication.java
@@ -0,0 +1,13 @@
+package com.jaas.jaasmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaasmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaasmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jaas/jaasmeta/JaasmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaas.jaasmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaasmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
